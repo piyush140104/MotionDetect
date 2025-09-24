fe465499d54ccbddd4d3635e706629d00aab511f
Piyush Gupta <piyushgupta140104@gmail.com>
2025-09-24 14:05:27 +0530

    changed allowed hosts

--- piyush_gupta/videotracker/videotracker/settings.py
+++ piyush_gupta/videotracker/videotracker/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-dev-key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST', 'localhost').split(',')
 
 # Application definition
 
